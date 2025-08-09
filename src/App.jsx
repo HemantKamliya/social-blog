e65e75e154e9991e87dc400cd6564a5a80febd2d
Hemant Kamliya <hemantkamliya87@gmail.com>
2025-08-09 17:26:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+   console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL)
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+  
+    </>
+  )
+}
+
+export default App
