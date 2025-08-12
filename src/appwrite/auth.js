2094f7e137965fca12028eae104b48eff47eef84
Hemant Kamliya <hemantkamliya87@gmail.com>
2025-08-12 22:35:15 +0530

    user authentication

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import confg from '../config/config'
+import { Client, Account, ID } from 'appwrite'
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(confg.appwriteUrl)
+            .setProject(confg.appwriteProjectIdc)
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                //call another method
+                this.login(email, password);
+
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            await this.account.createEmailPasswordSession(email, password);
+
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error;
+        }
+        return null;
+
+    }
+    
+    async logout() {
+        try {
+            await this.account.deleteSessions('current');
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+const authService = new AuthService();
+export default authService;
